b3ebdce681edca680811b5c7984bff4e0f6efd5d
matteo-didone <matteo.dido9@gmail.com>
2023-05-12 16:43:33 +0200

    Finished work

--- script.js
+++ script.js
@@ -2,30 +2,47 @@
 let numberOfKilometers = parseInt(prompt("How many kilometers do you want to run?"));
 let userAge = parseInt(prompt("How old are you?"));
 
+if
+(numberOfKilometers <= 0 || userAge <= 0 || isNaN(numberOfKilometers) || isNaN(userAge))
+{
+    alert("Please insert valid values, not the ones you provided");
+}
+
 // Create variables by using input values
 let ticketPrice = 0.21 * numberOfKilometers;
+let discountAmount;
 
 // Create if statements to calculate the price of the ticket
-if(userAge < 18)
+if (userAge < 18) 
 {
-    ticketPrice -= ((ticketPrice)/100)*20;
+    discountAmount = 0.20;
+    ticketPrice -= ticketPrice * discountAmount;
+    ticketPrice = ticketPrice.toFixed(2);
+    alert("Because you are under 18 years old, you get a 20% discount on your ticket price!");
+} else if (userAge > 65) {
+    discountAmount = 0.40;
+    ticketPrice -= ticketPrice * discountAmount;
+    ticketPrice = ticketPrice.toFixed(2);
+    alert("Because you are over 65 years old, you get a 40% discount on your ticket price!");
+} else if (userAge >= 18 && userAge <= 65) {
+    ticketPrice = ticketPrice.toFixed(2);
+    alert("Because you are between 18 and 65 years old, you don't get any discount on your ticket price.");
 }
 
-else if(userAge > 65)
-{
-    ticketPrice -= ((ticketPrice)/100)*40;
-}
-
-else if (userAge >= 18 && userAge <= 65)
-{
-    ticketPrice = ticketPrice;
-}
 
 // Create message
-let messageKilometersAndAge = "You want to run for" + numberOfKilometers + ". You are " + userAge + " years old, therefore the price of your ticket is going to be " + ticketPrice + "€";
+let messageKilometersAndAge = "Just to recap, you want to run for " + numberOfKilometers + " kilometers and you are " + userAge + " years old" + "\n" + "The price of your ticket is going to be " + ticketPrice + "€";
+let messageWrongParameters = "Because the parameters you provided were not valid, we couldn't calculate the price of your ticket";
 
 // Display message regarding kilometers, age and potential discount
-alert(message);
+if(numberOfKilometers > 0 && userAge > 0 && !isNaN(numberOfKilometers) && !isNaN(userAge))
+{
+    alert(messageKilometersAndAge);
+}
+else if (isNaN(numberOfKilometers) || isNaN(userAge))
+{
+    alert(messageWrongParameters);
+}
 
 // Get elements by id
 let kmParagraph = document.getElementById("km");
@@ -33,6 +50,16 @@ let userAgeParagraph = document.getElementById("userAge");
 let ticketPriceParagraph = document.getElementById("ticketPrice");
 
 // Update the content using innerHTML property
-kmParagraph.innerHTML = "Kilometers you want to run for " + numberOfKilometers;
-userAgeParagraph.innerHTML = "User Age: " + userAge;
-ticketPriceParagraph.innerHTML = "Ticket Price: " + ticketPrice + "€";
+if(numberOfKilometers > 0 && userAge > 0 && !isNaN(numberOfKilometers) && !isNaN(userAge))
+{
+    kmParagraph.innerHTML = "Kilometers you want to run for: " + numberOfKilometers;
+    userAgeParagraph.innerHTML = "User Age: " + userAge;
+    ticketPriceParagraph.innerHTML = "Ticket Price: " + ticketPrice + "€";
+}
+else if (isNaN(numberOfKilometers) || isNaN(userAge)) 
+{
+    kmParagraph.innerHTML = "Kilometers you want to run for is not valid";
+    userAgeParagraph.innerHTML = "The user's age is not valid";
+    ticketPriceParagraph.innerHTML = "The ticket price is not calculable";
+}
+
